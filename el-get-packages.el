1f78ff93893d30d734010f3a8d85b1f0d532121c
Norbert Wojtowicz <wojtowicz.norbert@gmail.com>
2011-01-28 14:30:22 +0100

    fixed build errors

--- el-get-packages.el
+++ el-get-packages.el
@@ -2,16 +2,16 @@
 (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
 (package-initialize)
 
+(add-to-list 'load-path "~/.emacs.d")
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
 (load "el-get-package-hooks.el")
 
 (setq el-get-sources
-      '(el-get rainbow-mode
-        (:name ruby-mode 
-               :type elpa
-               :load "ruby-mode.el"
-               :after (lambda () (ruby-mode-hook)))
+      ;; '(el-get rainbow-mode
+      '((:name ruby-mode 
+         :type elpa
+         :after (lambda () (ruby-mode-hook)))
         (:name inf-ruby  :type elpa)
         (:name ruby-compilation :type elpa)
         (:name css-mode 
@@ -19,23 +19,23 @@
                :after (lambda () (css-mode-hook)))
         (:name textmate
                :type git
-               :url "git://github.com/defunkt/textmate.el"
+               :url "https://github.com/defunkt/textmate.el.git"
                :load "textmate.el"
                :after (lambda () (textmate-mode-hook)))
         (:name rvm
                :type git
-               :url "http://github.com/djwhitt/rvm.el.git"
+               :url "https://github.com/djwhitt/rvm.el.git"
                :load "rvm.el"
                :compile ("rvm.el")
                :after (lambda () (rvm-autodetect-ruby)))
         (:name rhtml
                :type git
-               :url "https://github.com/eschulte/rhtml.git"
+               :url "https://github.com/crazycode/rhtml.git"
                :features rhtml-mode
                :after (lambda () (rhtml-mode-hook)))
         (:name yaml-mode 
                :type git
-               :url "http://github.com/yoshiki/yaml-mode.git"
+               :url "https://github.com/yoshiki/yaml-mode.git"
                :features yaml-mode
                :after (lambda () (yaml-mode-hook)))
 	))
